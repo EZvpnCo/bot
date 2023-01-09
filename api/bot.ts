80e08b830e797713546f49e10c133738be598ef7
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-10 03:16:33 +0330

    buttons

--- api/bot.ts
+++ api/bot.ts
@@ -4,13 +4,34 @@ const bot = new Bot("5817494017:AAE--FH-fCndLpZzrBDg_quJxuRa29SVVzc");
 
 // Handle the /start command.
 bot.command("start", (ctx) => {
+    let text = 'سلام به <b>EZvpn</b> خوش اومدید :)';
+    text += '<br> جهت استفاده از ربات بر روی /menu کلیک کنید';
+    ctx.reply(text, { parse_mode: "HTML" })
+});
+
+// Handle the /start command.
+bot.command("menu", (ctx) => {
     const inlineKeyboard = new InlineKeyboard()
-        .text("Get Me", "random").row()
-    ctx.reply("fgdh :)", { reply_markup: inlineKeyboard, })
+        .text("تعرفه ها", "prices")
+        .row()
+        .text("دانلود", "downloads")
+        .text("آموزش ها", "tutorials")
+        .row()
+        .text("عیب یابی", "diagnosis")
+        .text("سوالات متداول", "faq")
+        .row()
+        .text("لیست سرورها", "servers")
+        .row()
+        .url(
+        "پشتیبانی",
+        "EZvpnAdmin.t.me",
+        )
+    let text = 'از منوی زیر انتخاب کنید:';
+    ctx.reply(text, { reply_markup: inlineKeyboard, parse_mode: "HTML" })
 });
 
 // Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message", (ctx) => ctx.reply("میفهمم اما متوجه نمیشم :("));
 
 // Start the bot.
 bot.start();
