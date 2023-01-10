bba20a3d388af743870f71649178e6cdc22bc307
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-11 00:15:55 +0330

    seprate files

--- api/mainMenu.ts
+++ api/mainMenu.ts
@@ -2,7 +2,7 @@ import { Bot, InlineKeyboard } from "grammy";
 
 const MainMenu = (bot: Bot) => {
   // text
-  const mainMenuText = "از منوی زیر انتخاب کنید:";
+  const mainMenuText = "🔻 از منوی زیر انتخاب کنید:";
 
   // keyboard
   const mainMenuKeyboard = new InlineKeyboard().text("تعرفه ها", "prices").row().text("دانلود", "downloads").text("آموزش ها", "tutorials").row().text("عیب یابی", "diagnosis").text("سوالات متداول", "faq").row().text("لیست سرورها", "servers").row().url("پشتیبانی", "EZvpnAdmin.t.me");
