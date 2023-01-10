c6d86f7b3678cbb92754bc4816e9242df39290a5
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-11 00:39:44 +0330

    main menu emoji

--- api/mainMenu.ts
+++ api/mainMenu.ts
@@ -1,21 +1,32 @@
 import { Bot, InlineKeyboard } from "grammy";
 
 const MainMenu = (bot: Bot) => {
-  // text
-  const mainMenuText = "🔻 از منوی زیر انتخاب کنید:";
+    // text
+    const mainMenuText = "🔻 از منوی زیر انتخاب کنید:";
 
-  // keyboard
-  const mainMenuKeyboard = new InlineKeyboard().text("تعرفه ها", "prices").row().text("دانلود", "downloads").text("آموزش ها", "tutorials").row().text("عیب یابی", "diagnosis").text("سوالات متداول", "faq").row().text("لیست سرورها", "servers").row().url("پشتیبانی", "EZvpnAdmin.t.me");
+    // keyboard
+    const mainMenuKeyboard = new InlineKeyboard()
+        .text("💰 تعرفه‌ها", "prices")
+        .row()
+        .text("📥 مرکز دانلود", "downloads")
+        .text("📚 بخش آموزش", "tutorials")
+        .row()
+        .text("⚙️ عیب‌یابی", "diagnosis")
+        .text("❓سوالات متداول", "faq")
+        .row()
+        .text("🖥 سرورها", "servers")
+        .row()
+        .url("💬 پشتیبانی", "EZvpnAdmin.t.me");
 
-  // Handle the /menu command.
-  bot.command("menu", (ctx) => {
-    ctx.reply(mainMenuText, { reply_markup: mainMenuKeyboard });
-  });
+    // Handle the /menu command.
+    bot.command("menu", (ctx) => {
+        ctx.reply(mainMenuText, { reply_markup: mainMenuKeyboard });
+    });
 
-  // mainMenu
-  bot.callbackQuery("mainMenu", async (ctx) => {
-    await ctx.editMessageText(mainMenuText, { reply_markup: mainMenuKeyboard });
-  });
+    // mainMenu
+    bot.callbackQuery("mainMenu", async (ctx) => {
+        await ctx.editMessageText(mainMenuText, { reply_markup: mainMenuKeyboard });
+    });
 };
 
 export default MainMenu;
