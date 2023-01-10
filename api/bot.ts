1ea3c20f8ad40d104aa48c3888e3e146a9f47df2
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-10 03:59:08 +0330

    queries

--- api/bot.ts
+++ api/bot.ts
@@ -2,16 +2,8 @@ import { Bot, InlineKeyboard } from "grammy";
 
 const bot = new Bot("5817494017:AAE--FH-fCndLpZzrBDg_quJxuRa29SVVzc");
 
-// Handle the /start command.
-bot.command("start", (ctx) => {
-    let text = 'سلام به *EZvpn* خوش اومدید :)';
-    text += '\nجهت استفاده از ربات بر روی /menu کلیک کنید';
-    ctx.reply(text)
-});
-
-// Handle the /menu command.
-bot.command("menu", (ctx) => {
-    const inlineKeyboard = new InlineKeyboard()
+// Define keyboards
+const mainKeyboard = new InlineKeyboard()
         .text("تعرفه ها", "prices")
         .row()
         .text("دانلود", "downloads")
@@ -22,16 +14,66 @@ bot.command("menu", (ctx) => {
         .row()
         .text("لیست سرورها", "servers")
         .row()
-        .url(
-        "پشتیبانی",
-        "EZvpnAdmin.t.me",
-        )
+        .url("پشتیبانی", "EZvpnAdmin.t.me")
+
+
+// Handle the /start command.
+bot.command("start", (ctx) => {
+    let text = 'سلام به *EZvpn* خوش اومدید :)';
+    text += '\nجهت استفاده از ربات بر روی /menu کلیک کنید';
+    ctx.reply(text)
+});
+
+// Handle the /menu command.
+bot.command("menu", (ctx) => {
     let text = 'از منوی زیر انتخاب کنید:';
-    ctx.reply(text, { reply_markup: inlineKeyboard })
+    ctx.reply(text, { reply_markup: mainKeyboard })
 });
 
 // Handle other messages.
 bot.on("message", (ctx) => ctx.reply("میفهمم اما متوجه نمیشم :("));
+
+// prices
+bot.callbackQuery("prices", async (ctx) => {
+  await ctx.answerCallbackQuery({
+    text: "You were curious, indeed!",
+  });
+});
+
+// downloads
+bot.callbackQuery("downloads", async (ctx) => {
+  await ctx.answerCallbackQuery({
+    text: "You were curious, indeed!",
+  });
+});
+
+// tutorials
+bot.callbackQuery("tutorials", async (ctx) => {
+  await ctx.answerCallbackQuery({
+    text: "You were curious, indeed!",
+  });
+});
+
+// diagnosis
+bot.callbackQuery("diagnosis", async (ctx) => {
+  await ctx.answerCallbackQuery({
+    text: "You were curious, indeed!",
+  });
+});
+
+// faq
+bot.callbackQuery("faq", async (ctx) => {
+  await ctx.answerCallbackQuery({
+    text: "You were curious, indeed!",
+  });
+});
+
+// servers
+bot.callbackQuery("servers", async (ctx) => {
+  await ctx.answerCallbackQuery({
+    text: "You were curious, indeed!",
+  });
+});
 
 // Start the bot.
 bot.start();
