827644adcebf7422aead61e8f13ea096b1f39e21
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-11 14:26:49 +0330

    management menu

--- api/admin/index.ts
+++ api/admin/index.ts
@@ -1,13 +1,28 @@
-import { Bot, } from "grammy";
+import { Bot, InlineKeyboard, } from "grammy";
 import { Admins as admins } from "../config";
 
 const Admin = (bot: Bot) => {
 
+    const keyboard = new InlineKeyboard()
+        .text("📡 سرورها", "admin:servers")
+        .text("📟 نودها", "admin:nodes")
+        .row()
+        .text("👥 کاربران", "admin:users")
+        .row()
+        .text("صفحه اصلی 🏠", "mainMenu")
+
     // Handle the /ping command.
     bot.command("management", async (ctx) => {
-        // if ()
-        const text = `Hello Admin`
-        await ctx.reply(text, { parse_mode: 'MarkdownV2', reply_to_message_id: ctx.msg.message_id });
+        let _text, _keyboard
+        if (!admins.includes(ctx?.from?.id!)) {
+            _text = `شما دسترسی های لازم رو نداری 🚫`
+        }
+        else {
+            _text = `به بخش مدیریت خوش اومدی
+            چیکار میتونم انجام بدم برات؟`
+            _keyboard = keyboard
+        }
+        await ctx.reply(_text, { reply_markup: _keyboard });
     });
 };
 
