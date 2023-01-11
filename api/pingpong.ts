121ec9f8988ba81d8daac4b1c353b267264ccea1
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-11 12:48:51 +0330

    ping pong

--- api/pingpong.ts
+++ api/pingpong.ts
@@ -4,9 +4,11 @@ const PingPong = (bot: Bot) => {
 
     // Handle the /ping command.
     bot.command("ping", async (ctx) => {
-        const text = ` *Pong\\!*
-        *ChatID:* ${ctx.chat.id} *UserID:* ${ctx.message?.from.id}`
-        await ctx.reply(text, { parse_mode: 'MarkdownV2', reply_to_message_id: ctx.msg.message_id }).catch((e) => console.log(e));
+        const text = `*Pong\\!*
+*ChatID:* ${ctx.chat.id}
+*ChatType:* ${ctx.chat.type}
+*UserID:* ${ctx.message?.from.id}`
+        await ctx.reply(text, { parse_mode: 'MarkdownV2', reply_to_message_id: ctx.msg.message_id });
     });
 };
 
