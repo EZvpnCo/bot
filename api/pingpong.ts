0607cc1ba00f9f4a3f6317e065a99c599d4dda88
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-11 12:38:56 +0330

    try cach ping pong

--- api/pingpong.ts
+++ api/pingpong.ts
@@ -4,7 +4,7 @@ const PingPong = (bot: Bot) => {
 
     // Handle the /ping command.
     bot.command("ping", async (ctx) => {
-        const text = ` *Pong!* *ChatID:* ${ctx.chat.id} *UserID:* ${ctx.message?.from.id}`
+        const text = ` *Pong\!* *ChatID:* ${ctx.chat.id} *UserID:* ${ctx.message?.from.id}`
         await ctx.reply(text, { parse_mode: 'MarkdownV2', reply_to_message_id: ctx.msg.message_id }).catch((e) => console.log(e));
     });
 };
