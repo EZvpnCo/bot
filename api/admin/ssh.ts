331a76fdbd5271fed3e02dec4cbe75d74707ac69
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-12 23:50:40 +0330

    ssh exec

--- api/admin/ssh.ts
+++ api/admin/ssh.ts
@@ -19,6 +19,7 @@ export const liveSSH = async (config: Config, command: string, callback: { (resu
         try {
             await ssh.connect(config)
             ssh.execCommand(command, {
+                cwd: '',
                 onStdout: (chunk) => {
                     callback('StdOut:\n' + chunk.toString('utf8'))
                 },
