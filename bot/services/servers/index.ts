4ad0ab19373186c554b75656d60a26010b55ef4f
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-02 22:43:16 +0330

    fix servers

--- bot/services/servers/index.ts
+++ bot/services/servers/index.ts
@@ -47,7 +47,7 @@ class ServersService {
             else emj = "🔴"
             _ser += `${emj} *${name}* ${online_user} ${node_class}`
         }
-        return `*لیست سرورها (${this.data.length}):*`
+        return `*لیست سرورها (${this.data.length}:*`
     }
 
     private response = async (ctx: MyContext) => {
