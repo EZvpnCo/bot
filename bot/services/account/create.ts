e27bf023d9ce8bfb5c6ac8b2953a17cfc8ab55bb
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-05 22:14:33 +0330

    fixregister & inp state

--- bot/services/account/create.ts
+++ bot/services/account/create.ts
@@ -62,7 +62,7 @@ class AccountCreateService {
                 new MenuService(this.bot).response(ctx)
             } catch (error) {
                 await ctx.reply("❌ خطایی در روند ثبت نام رخ داده است با پشتیبانی در ارتباط باشید");
-                // await ctx.reply("Error: " + JSON.stringify(error))
+                await ctx.reply("Error: " + JSON.stringify(error.data))
                 console.log(error)
                 new MenuService(this.bot).response(ctx)
             }
