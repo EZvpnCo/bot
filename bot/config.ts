adf4eeca2a94f1480a0ad20bc2a7a07f21b91fff
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-03-03 17:51:10 +0330

    fixrecipt

--- bot/config.ts
+++ bot/config.ts
@@ -1,4 +1,4 @@
 
 export const BotToken = "5809126302:AAEvo0ECMTbs-7VHQPfjWfOtofBe231xHhg"
 export const SuperAdmin = 115025624
-export const AdminGP = 115025624
+export const AdminGP = -1001424073479
