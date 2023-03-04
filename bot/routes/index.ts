e7f234e5f6c4df3cfe48bf82509c0a6f9760978d
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-03-05 00:31:41 +0330

    fix payment

--- bot/routes/index.ts
+++ bot/routes/index.ts
@@ -31,7 +31,7 @@ export default function EndPoint(bot: MyBot) {
         const qr = await QRCode.toFile("temp/" + filename, suburl)
         console.log(qr)
         // const temp = fs.readFileSync(__dirname + "/temp/" + filename)
-        res.sendFile("temp/" + filename)
+        res.sendFile("temp/1677963682093.png")
         // const img = `data:image/png;base64,${qr.toString('base64')}`
 
         // res.send(`
