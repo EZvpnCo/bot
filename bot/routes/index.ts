a1da58a8b2d460bcb7b585b8154fd119ca816269
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-03-05 00:34:45 +0330

    fix payment

--- bot/routes/index.ts
+++ bot/routes/index.ts
@@ -62,6 +62,7 @@ export default function EndPoint(bot: MyBot) {
             //     </html>
             // `)
         } catch (err) {
+            console.log(err)
             res.send("Error")
         }
     })
