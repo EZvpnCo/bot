d4b95611f6eb4fe5bc8dbcc8887b35eef6a931a2
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-05-25 00:27:50 +0330

    fix private

--- bot/services/private/index.ts
+++ bot/services/private/index.ts
@@ -9,11 +9,11 @@ class PrivateService {
     }
 
     public run() {
-        this.bot.command("start", (ctx) => {
-            const isNew = ctx.session.isNew
-            const text = isNew ? ctx.t("welcome") : ctx.t("welcome-back");
-            ctx.reply(text, { parse_mode: 'MarkdownV2' }).catch(e => console.log(e));
-        });
+        // this.bot.command("start", (ctx) => {
+        //     const isNew = ctx.session.isNew
+        //     const text = isNew ? ctx.t("welcome") : ctx.t("welcome-back");
+        //     ctx.reply(text, { parse_mode: 'MarkdownV2' }).catch(e => console.log(e));
+        // });
         // new MenuService(this.bot).run()
     }
 
